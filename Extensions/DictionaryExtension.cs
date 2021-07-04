06524dd41370d1efa41b68aabc6900f1b3f79ea0
George Svitenko <georg.svit@gmail.com>
2021-07-04 16:44:58 +0300

    Implement an extension method for custom dictionary concatenation

---
+++ Extensions/DictionaryExtension.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+
+namespace WebsiteCrawler.Extensions
+{
+    public static class DictionaryExtension
+    {
+        public static void CustomConcat<Tkey, TValue>(this Dictionary<Tkey, TValue> baseDict, Dictionary<Tkey, TValue> supplementDict)
+        {
+            foreach (var item in supplementDict)
+                baseDict.TryAdd(item.Key, item.Value);
+        }
+    }
+}
