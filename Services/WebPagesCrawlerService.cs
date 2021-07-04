2fd0f565a00674ee87f9c6a7f82977c4c96bbc1a
George Svitenko <georg.svit@gmail.com>
2021-07-04 15:02:50 +0300

    Add WebPagesCrawlerService class and implement GetLinksFromWebPage method

---
+++ Services/WebPagesCrawlerService.cs
@@ -0,0 +1,26 @@
+﻿using AngleSharp.Html.Parser;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebsiteCrawler.Services
+{
+    public static class WebPagesCrawlerService
+    {
+        private static Dictionary<string, TimeSpan> GetLinksFromWebPage(string pageData, string host)
+        {
+            HtmlParser parser = new();
+            var page = parser.ParseDocument(pageData);
+
+            var links = page.QuerySelectorAll("a")
+                            .Select(element => element.GetAttribute("href"))
+                            .Where(link => link is not null && (link.StartsWith(host) || link.StartsWith("/")) && !link.Contains("#"))
+                            .Select(link => link.StartsWith("/") ? $"{host}{link}" : link)
+                            .Distinct().ToDictionary(x => x, x => new TimeSpan());
+
+            return links;
+        }
+    }
+}
