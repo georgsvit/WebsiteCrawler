d12b3fff6be939b91d52306f81c72fa6cc6fa0de
George Svitenko <georg.svit@gmail.com>
2021-07-04 14:20:55 +0300

    Add GetFileDataAndResponseTimeByUri method to HttpService class

--- Services/HttpService.cs
+++ Services/HttpService.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Diagnostics;
 using System.IO;
 using System.Net;
 using System.Text;
@@ -37,5 +38,40 @@ namespace WebsiteCrawler.Services
                     throw new Exception($"Response status code: {response.StatusCode}");
             }
         }
-    }
+
+        public static (string, TimeSpan) GetFileDataAndResponseTimeByUri(string uri)
+        {
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(new Uri(uri));
+            Stopwatch timer = new();
+
+            timer.Start();
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+            timer.Stop();
+
+            switch (response.StatusCode)
+            {
+                case HttpStatusCode.OK:
+                    Stream receiveStream = response.GetResponseStream();
+                    StreamReader reader = string.IsNullOrWhiteSpace(response.CharacterSet) switch
+                    {
+                        false => new(receiveStream, Encoding.GetEncoding(response.CharacterSet)),
+                        _ => new(receiveStream)
+                    };
+
+                    string data = reader.ReadToEnd();
+
+                    response.Close();
+                    reader.Close();
+
+                    if (String.IsNullOrWhiteSpace(data))
+                        throw new Exception("File is empty");
+
+                    return (data, timer.Elapsed);
+
+                default:
+                    response.Close();
+                    throw new Exception($"Response status code: {response.StatusCode}");
+            }
+        }
+    }    
 }
