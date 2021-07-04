bbda4969c00f927794bf186534c1311dca4763bb
George Svitenko <georg.svit@gmail.com>
2021-07-04 17:52:57 +0300

    Add GetLinksFromSitemap method to SitemapCrawlerService class

--- Services/SitemapCrawlerService.cs
+++ Services/SitemapCrawlerService.cs
@@ -1,11 +1,29 @@
 ﻿using System;
 using System.Linq;
 using System.Text.RegularExpressions;
+using System.Xml;
 
 namespace WebsiteCrawler.Services
 {
     public static class SitemapCrawlerService
     {
+        private static (string[], string[]) GetLinksFromSitemap(string uri)
+        {
+            string sitemapData = HttpService.GetFileDataByUri(uri);
+
+            XmlDocument xml = new();
+            xml.LoadXml(sitemapData);
+
+            XmlNodeList nodesList = xml.GetElementsByTagName("loc");
+
+            string[] links = nodesList.Cast<XmlElement>().Select(element => element.InnerText).ToArray();
+            string[] sitemaps = links.Where(link => link.Contains("sitemap") && link.EndsWith("xml")).ToArray();
+            links = links.Except(sitemaps).ToArray();
+
+            return (links, sitemaps);
+        }
+
+
         private static string[] GetSitemapLinksFromRobots(string uri)
         {
             string robotsData = HttpService.GetFileDataByUri($"{uri}robots.txt");
