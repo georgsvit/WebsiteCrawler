8a05cdc271fb8fd54ad65218cbd80c7c5764995b
George Svitenko <georg.svit@gmail.com>
2021-07-04 16:47:02 +0300

    Add Crawl method to WebPagesCrawlerService class Improve link filtering rule

--- Services/WebPagesCrawlerService.cs
+++ Services/WebPagesCrawlerService.cs
@@ -2,14 +2,42 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
 using System.Text.RegularExpressions;
-using System.Threading.Tasks;
+using WebsiteCrawler.Extensions;
 
 namespace WebsiteCrawler.Services
 {
     public static class WebPagesCrawlerService
     {
+        public static Dictionary<string, TimeSpan> Crawl(string uri)
+        {
+            Dictionary<string, TimeSpan> links = new()
+            {
+                [uri] = new()
+            };
+
+            string pathAndQuery = new Uri(uri).PathAndQuery;
+
+            if (pathAndQuery is not null && pathAndQuery != "/")
+            {
+                links.Add(uri.Replace(pathAndQuery, ""), new());
+            }
+
+            string currentLink = uri;
+
+            while (currentLink != null)
+            {
+                var (extractedLinks, responseTime) = ProcessPage(currentLink);
+
+                links[currentLink] = responseTime;
+                links.CustomConcat(extractedLinks);
+
+                currentLink = links.FirstOrDefault(item => item.Value.TotalMilliseconds == 0).Key;
+            }
+
+            return links;
+        }
+
         private static (Dictionary<string, TimeSpan>, TimeSpan) ProcessPage(string uri)
         {
             string host = uri.Substring(0, uri.IndexOf('/', 8));
@@ -27,7 +55,7 @@ namespace WebsiteCrawler.Services
 
             var links = page.QuerySelectorAll("a")
                             .Select(element => element.GetAttribute("href"))
-                            .Where(link => link is not null && (link.StartsWith(host) || link.StartsWith("/")) && !link.Contains("#"))
+                            .Where(link => link is not null && (link.StartsWith(host) || link.StartsWith("/")) && !link.Contains("#") && !link.Contains("@"))
                             .Select(link => link.StartsWith("/") ? $"{host}{link}" : link)
                             .Distinct().ToDictionary(x => x, x => new TimeSpan());
 
@@ -48,7 +76,7 @@ namespace WebsiteCrawler.Services
             MatchCollection matchCollection = rule.Matches(pageData);
 
             var links = matchCollection.Select(match => match.Groups[2].ToString())
-                            .Where(link => link is not null && (link.StartsWith(host) || link.StartsWith("/")) && !link.Contains("#"))
+                            .Where(link => link is not null && (link.StartsWith(host) || link.StartsWith("/")) && !link.Contains("#") && !link.Contains("@"))
                             .Select(link => link.StartsWith("/") ? $"{host}{link}" : link)
                             .Distinct().ToDictionary(x => x, x => new TimeSpan());
 
